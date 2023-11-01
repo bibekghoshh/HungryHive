91677026a6e85024c466e1cbbb2e07536d89d5ab
bibekghoshh <bibek7478@gmail.com>
2023-11-01 18:54:03 +0530

    lazy loading

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import Header from "./src/components/Header";
 import "./index.css";
 import Body from "./src/components/Body";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-import About from "./src/components/About";
-import ItemCart from "./src/components/ItemCart";
 import Error from "./src/components/Error";
-import RestaurantMenu from "./src/components/RestaurantMenu";
 import UserContextProvider from "./src/context/UserContextProvider";
 import { Provider } from "react-redux";
 import appStore from "./src/redux/appStore";
+import { lazy } from "react";
+import { Suspense } from "react";
+import RestaurantMenuShimmerUi from "./src/components/shimmerUi/RestaurantMenuShimmerUi";
+
+const About=lazy(()=>import("./src/components/About"));
+const ItemCart=lazy(()=>import("./src/components/ItemCart"));
+const RestaurantMenu=lazy(()=>import("./src/components/RestaurantMenu"));
 
 const App = () => {
   return (
@@ -33,15 +37,15 @@ const appRoute = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: <Suspense fallback={<div>Loading...</div>}><About/></Suspense>,
       },
       {
         path: "/cart",
-        element: <ItemCart />,
+        element:<Suspense fallback={<div>Loading...</div>}><ItemCart/></Suspense>,
       },
       {
         path: "/restaurants/:resId",
-        element: <RestaurantMenu/>,
+        element: <Suspense fallback={<RestaurantMenuShimmerUi/>}><RestaurantMenu/></Suspense>,
       },
     ],
     errorElement: <Error/>,
